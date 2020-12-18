c574eca94355afe2c81bdfb70d9f33d0cff98744
pkdc <pkdchoy@gmail.com>
2020-12-18 13:14:57 +0000

    Added the functionality hiding and showing the contact form in the landing page through JS. Removed some <br>s and used divs seperate lines, and css to add margins, still not mobile responsive yet

---
+++ app.js
@@ -0,0 +1,9 @@
+// document.getElementById('expand-form').addEventListener('click', expand);
+//
+// function expand() {
+//   document.querySelector('.form').classList.toggle('show');
+// }
+
+document.getElementById("expand-form").addEventListener('click', () => {
+  document.querySelector('.form').classList.toggle('show');
+});
