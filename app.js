7d20c6950a9bfce1fc09e59b20d53dfd4fc2a9f6
pkdc <pkdchoy@gmail.com>
2020-12-31 14:24:15 +0000

    added if-else checks to check the display value of the form before toggling. Removed .hide from the css, and made some style changes to the colour of hovering input fields.

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-// document.getElementById('expand-form').addEventListener('click', expand);
-//
-// function expand() {
-//   document.querySelector('.form').classList.toggle('show');
-// }
+const expandForm = document.querySelector(".green-bkg-btn");
+const form = document.querySelector(".form");
 
-document.getElementById("expand-form-btn").addEventListener('click', () => {
-  document.getElementById("expand-form-btn").classList.toggle("change-colour-grey");
-  document.querySelector('.form').classList.toggle('show');
+// adding event Listener to the btn
+expandForm.addEventListener('click', () => {
+  const formStyle = window.getComputedStyle(form);
+  const formDisplay = formStyle.getPropertyValue('display');
+
+  // display it if hidden
+  if (formDisplay === "none") {
+    expandForm.classList.toggle("change-colour-grey");
+    form.classList.toggle('show');
+  }
+
+  // hide it if shown
+  else if (formDisplay === "block") {
+    expandForm.classList.toggle("change-colour-white");
+    form.classList.toggle('show');
+  }
+
+  // just in case there is a bug and introduced another display value
+  else {
+      form.classList.toggle('show');
+  }
 });
