a05fdf5ae17d15caf7de3757547840a8c07b8609
pkdc <pkdchoy@gmail.com>
2021-05-24 14:05:26 +0100

    Changed some selectors, changed some styling (esp to the research/reference page)

--- app.js
+++ app.js
@@ -2,43 +2,39 @@
 const navBarBtn = document.querySelector(".nav-bar-btn-mob");
 const navBar = document.getElementById("nav");
 
+navBarBtn.addEventListener("click", () => {
+  // const navStyle = window.getComputedStyle('navBar');
+  // const navDisplay = navStyle.getPropertyValue('display');
 
-navBarBtn.addEventListener('click', () => {
+  // console.log(window.getComputedStyle(navBarBtn));
+  // navBarBtn.classList.toggle("change-colour-grey");
+  navBarBtn.classList.toggle("nav-bar-btn-active");
+  navBar.classList.toggle("show");
 
-    // const navStyle = window.getComputedStyle('navBar');
-    // const navDisplay = navStyle.getPropertyValue('display');
+  // console.log(navDisplay);
 
-    // console.log(window.getComputedStyle(navBarBtn));
-    // navBarBtn.classList.toggle("change-colour-grey");
-    navBarBtn.classList.toggle('nav-bar-btn-active');
-    navBar.classList.toggle('show');
-
-    // console.log(navDisplay);
-
-    // if (navDisplay === "none") {
-    //     navBarBtn.style.display = "block";
-    //     navBarBtn.style.backgroundColor = "var (--thymynd-green, #2ECC71)";
-    //     navBarBtn.style.color = "white";
-    // } else {
-    //     navBarBtn.style.display = "none";
-    //     navBarBtn.style.backgroundColor = "white";
-    //     navBarBtn.style.color = "var (--thymynd-green, #2ECC71)";
-    // }
-
+  // if (navDisplay === "none") {
+  //     navBarBtn.style.display = "block";
+  //     navBarBtn.style.backgroundColor = "var (--thymynd-green, #2ECC71)";
+  //     navBarBtn.style.color = "white";
+  // } else {
+  //     navBarBtn.style.display = "none";
+  //     navBarBtn.style.backgroundColor = "white";
+  //     navBarBtn.style.color = "var (--thymynd-green, #2ECC71)";
+  // }
 });
-
 
 // code for expand-form-btn
 const expandForm = document.querySelector(".green-bkg-btn");
 const form = document.querySelector(".form");
 
 // adding event Listener to the btn
-expandForm.addEventListener('click', () => {
+expandForm.addEventListener("click", () => {
   // const formStyle = window.getComputedStyle(form);
   // const formDisplay = formStyle.getPropertyValue('display');
 
   expandForm.classList.toggle("change-colour-grey");
-  form.classList.toggle('show');
+  form.classList.toggle("show");
 
   // display it if it's hidden
   // if (formDisplay === "none") {
