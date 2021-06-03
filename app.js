09345bfb3d281dfb606554f0d1d618533eb1d06c
pkdc <pkdchoy@gmail.com>
2021-06-03 13:30:39 +0100

    Modified the organisation of the header, put the mobile menu icon to the front with large z-index, flex-box in the nav, given more relevant id and class names to elements.

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 //  code for nav-bar-btn-mob
 "use strict";
 
-const navBarBtn = document.querySelector(".nav-bar-btn-mob");
-const navBar = document.getElementById("nav");
+const navBarBtn = document.getElementById("nav-bar-btn-mob");
+const navBar = document.getElementById("nav-bar-links");
+console.log(navBarBtn);
+console.log(navBar);
 
 navBarBtn.addEventListener("click", () => {
-  // const navStyle = window.getComputedStyle('navBar');
-  // const navDisplay = navStyle.getPropertyValue('display');
+  // can't get in here
+  // console.log("added");
+  const navStyle = window.getComputedStyle(navBar);
+  const navDisplay = navStyle.getPropertyValue("display");
 
   // console.log(window.getComputedStyle(navBarBtn));
   // navBarBtn.classList.toggle("change-colour-grey");
+
   navBarBtn.classList.toggle("nav-bar-btn-active");
+  // console.log(navBar);
   navBar.classList.toggle("show");
 
-  // console.log(navDisplay);
-
-  // if (navDisplay === "none") {
-  //     navBarBtn.style.display = "block";
-  //     navBarBtn.style.backgroundColor = "var (--thymynd-green, #2ECC71)";
-  //     navBarBtn.style.color = "white";
-  // } else {
-  //     navBarBtn.style.display = "none";
-  //     navBarBtn.style.backgroundColor = "white";
-  //     navBarBtn.style.color = "var (--thymynd-green, #2ECC71)";
-  // }
+  console.log(navDisplay);
 });
 
 // code for expand-form-btn
-const expandForm = document.querySelector(".green-bkg-btn");
+const expandForm = document.getElementById("expand-form-btn");
 const form = document.querySelector(".form");
-
 // adding event Listener to the btn
 expandForm.addEventListener("click", () => {
   // const formStyle = window.getComputedStyle(form);
@@ -37,16 +32,4 @@ expandForm.addEventListener("click", () => {
 
   expandForm.classList.toggle("change-colour-grey");
   form.classList.toggle("show");
-
-  // display it if it's hidden
-  // if (formDisplay === "none") {
-  //   expandForm.classList.toggle("change-colour-grey");
-  //   form.classList.toggle('show');
-  // }
-  //
-  // // hide it if it's shown, and also if there is a bug introducing another display value i.e. not none
-  // else if (formDisplay === "block") {
-  //   expandForm.classList.toggle("change-colour-grey");
-  //   form.classList.toggle('show');
-  // }
 });
