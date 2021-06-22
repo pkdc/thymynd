19cb728bc619bede0bda404a98580bb1bfdcbcd5
pkdc <pkdchoy@gmail.com>
2021-06-23 00:08:04 +0100

    Mainly Modified ref.html and the corresponding styles. changed the class names of the elements in ref to more meaningful/ less confusing names, debugged html in ref, used grid to layout the ref table in desktop size

--- app.js
+++ app.js
@@ -7,19 +7,19 @@ console.log(navBarBtn);
 console.log(navBar);
 
 navBarBtn.addEventListener("click", () => {
-  // can't get in here
-  // console.log("added");
-  const navStyle = window.getComputedStyle(navBar);
-  const navDisplay = navStyle.getPropertyValue("display");
+	// can't get in here
+	// console.log("added");
+	const navStyle = window.getComputedStyle(navBar);
+	const navDisplay = navStyle.getPropertyValue("display");
 
-  // console.log(window.getComputedStyle(navBarBtn));
-  // navBarBtn.classList.toggle("change-colour-grey");
+	// console.log(window.getComputedStyle(navBarBtn));
+	// navBarBtn.classList.toggle("change-colour-grey");
 
-  navBarBtn.classList.toggle("nav-bar-btn-active");
-  // console.log(navBar);
-  navBar.classList.toggle("show-menu");
+	navBarBtn.classList.toggle("nav-bar-btn-active");
+	// console.log(navBar);
+	navBar.classList.toggle("show-menu");
 
-  console.log(navDisplay);
+	console.log(navDisplay);
 });
 
 // code for expand-form-btn
@@ -27,9 +27,9 @@ const expandForm = document.getElementById("expand-form-btn");
 const form = document.querySelector(".form");
 // adding event Listener to the btn
 expandForm.addEventListener("click", () => {
-  // const formStyle = window.getComputedStyle(form);
-  // const formDisplay = formStyle.getPropertyValue('display');
+	// const formStyle = window.getComputedStyle(form);
+	// const formDisplay = formStyle.getPropertyValue('display');
 
-  expandForm.classList.toggle("change-colour-grey");
-  form.classList.toggle("show-form");
+	expandForm.classList.toggle("change-colour-grey");
+	form.classList.toggle("show-form");
 });
