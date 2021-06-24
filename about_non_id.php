828b904217fd844a14f9e05441484db35a4aa756
pkdc <pkdchoy@gmail.com>
2021-06-24 13:18:47 +0100

    Styled the ref page, particularly the ref table, which is screen size responsive for 3 different screen sizes. Made some styling modifications to homepage. And also styled the about and contact pages. Ready to merge back to the master branch

--- about_non_id.php
+++ about_non_id.php
@@ -2,23 +2,23 @@
   include_once "includes/start_session.php";
   include_once "includes/header.php";
  ?>
-<div id="us">
+<div class="us">
   <h2>About Us</h2>
   <p>
-    <strong>ThyMynd<br>
-    Noun | /ðʌɪ . mʌɪnd/ | pronounced as Thy-mind</strong><br>
+    <h3>ThyMynd</h3>
+    <h4>Noun | /ðʌɪ . mʌɪnd/ | pronounced as Thy-mind</h4>
     We played with the words ‘Your Mind’, but ultimately Thymynd is about facilitating you for quality decision making.
   </p>
   <p>
-     The friends met at the College X years ago and have been in touch for badminton, food, travel (and a long list of random stuff) since then.<br>
-   One day, they deep-dived in the observations of personal resilience and mental health support at school levels. The discussion resonated with them so much that, they want to make Thymynd work for one and for all.
+     The friends met at the College X years ago and have been in touch for badminton, food, travel (and a long list of random stuff) since then.</p>
+   <p>One day, they deep-dived in the observations of personal resilience and mental health support at school levels. The discussion resonated with them so much that, they want to make Thymynd work for one and for all.
   </p>
 </div>
 
-<div id="team">
-    <img src="https://www.imdb.com/title/tt4154664/" alt="Captain Marvel">
+<div class="team">
+    <img  alt="Monique Ho">
 
-    <img src="https://en.wikipedia.org/wiki/Iron_Man" alt="Iron man">
+    <img  alt="David Choy">
 <?php
   include_once 'includes/footer.php';
 ?>
